b5ee4d4f02cfa4fb3b60bb3ef40c34085fc75882
Zeynab786 <zmmohamed@myseneca.ca>
2025-06-29 16:37:56 -0400

    sever.js chnaged

--- server.js
+++ server.js
@@ -19,19 +19,8 @@ const LegoData = require("./modules/legoSets");
 const legoData = new LegoData();
 
 const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
 
-// Debugging logs for runtime errors
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection:', reason);
-});
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
-});
-
-// Middleware for static files if you have any
-app.use(express.static("public"));
-
-// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
@@ -39,44 +28,39 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "about.html"));
 });
-
+legoData.initialize()
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server is listening on port ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to start server:", err);
+  });
 app.get("/lego/sets", async (req, res) => {
   try {
     const theme = req.query.theme;
-    const sets = theme
-      ? await legoData.getSetsByTheme(theme)
-      : await legoData.getAllSets();
-    res.json(sets);
+
+    if (theme) {
+      const sets = await legoData.getSetsByTheme(theme);
+      res.json(sets); 
+    } else {
+      const sets = await legoData.getAllSets();
+      res.json(sets); 
+    }
   } catch (err) {
-    res.status(404).json({ error: err.toString() });
+    res.status(404).json({ error: err });
   }
 });
-
 app.get("/lego/sets/:set_num", async (req, res) => {
   try {
     const setNum = req.params.set_num;
     const set = await legoData.getSetByNum(setNum);
-    res.json(set);
+    res.json(set); 
   } catch (err) {
-    res.status(404).json({ error: err.toString() });
+    res.status(404).json({ error: err });
   }
 });
-
-// 404 handler
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-
-// Serverless export
-let initialized = false;
-
-module.exports = async (req, res) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  if (!initialized) {
-    console.log("Initializing legoData...");
-    await legoData.initialize();
-    initialized = true;
-    console.log("legoData initialized.");
-  }
-  return app(req, res);
-};
